38f1a0e01023187960db5ab18d1f03a646866260
alce09 <alce09001@utb.vaxjo.se>
2026-09-11 13:26:18 +0200

    Gjort Uppgift2.1

---
+++ Uppgift2.1/Uppgift2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vad heter din favorit bok?");
+            string namnet = Console.ReadLine();
+
+            Console.WriteLine("Namnet på din bok är \"" + namnet + "\".");
+
+
+
+        }
+
+
+
+    }
+}
