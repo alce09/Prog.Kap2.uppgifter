b13929a9045e1c242db635f6a5ac67d3f43d05d4
alce09 <alce09001@utb.vaxjo.se>
2026-09-11 14:13:44 +0200

    Gjort Uppgift2.4

---
+++ Uppgift2.4/Uppgift2.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uppgift2._4
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hej startavgiften på bilen är 300kr");
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            int antldagar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många kilometer vill du köra?");
+            float km = float.Parse(Console.ReadLine());
+
+            float C = 300 + km + 500 * (antldagar - 1);
+
+            Console.WriteLine("Summan du ska betala är " + C + "kronor");
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
