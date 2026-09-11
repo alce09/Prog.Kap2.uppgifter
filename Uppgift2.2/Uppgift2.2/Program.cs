bf7c0dd2089f0b3dbcf101d31054eadbc1f1ff37
alce09 <alce09001@utb.vaxjo.se>
2026-09-11 13:46:19 +0200

    Gjort Uppgift2.2

---
+++ Uppgift2.2/Uppgift2.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uppgift2._2
+{
+    class Program
+    {   
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Alma hoppade och slog världsrekordet i höjdhopp, hon hoppade 10 meter.");
+            Console.WriteLine("Hur högt kan du hoppa?");
+            string höjden = Console.ReadLine();
+
+            float höjdslut = float.Parse(höjden);
+
+            float summa = 10 - höjdslut;
+
+            Console.WriteLine("Alma kan hoppa " + summa + "meter längre en dig:)");
+            
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
